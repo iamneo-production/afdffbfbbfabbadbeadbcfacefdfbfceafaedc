54b9d9105160b49da1e2678df68108b83ec16e61
Iamneo <techadmin@iamneo.ai>
2023-09-04 09:52:46 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/TaskNotFoundException.java
+++ springapp/src/main/java/com/examly/springapp/TaskNotFoundException.java
@@ -3,7 +3,7 @@ package com.examly.springapp;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value=HttpStatus.OK)
+@ResponseStatus(value = HttpStatus.OK)
 public class TaskNotFoundException extends RuntimeException {
     public TaskNotFoundException(String message) {
         super(message);
